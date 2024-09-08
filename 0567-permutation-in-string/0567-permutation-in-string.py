a3ec069ea27f8165ec30e234ecb5a92ea94427e9
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-08 10:01:54 -0400

    Time: 3342 ms (7.36%), Space: 16.6 MB (87.20%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,15 +1,13 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        def sort_string(s: str) -> str:
+        def sort_string(s:str) -> str:
             return ''.join(sorted(s))
-        
+
         s1_sorted = sort_string(s1)
         
         # Ensure that the loop checks all valid starting positions for a substring in s2 that has the same length as s1.
         for i in range(len(s2) - len(s1) + 1):
-            if s1_sorted == sort_string(s2[i:i + len(s1)]):
+            if s1_sorted == sort_string(s2[i: i+len(s1)]):
                 return True
-        
         return False
-
         
